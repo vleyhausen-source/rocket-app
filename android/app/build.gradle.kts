cde3d3523f61ddc3cb87d38ba2a0fe2dc3062b88
Rocket Dev <dev@rocket-app.dev>
2026-06-14 16:04:45 +0200

    Bump version to 1.0.13 (versionCode 15)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 14
-        versionName = "1.0.12"
+        versionCode = 15
+        versionName = "1.0.13"
     }
 
     buildTypes {
